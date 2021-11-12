65670227b746c220ca24982f4a62ec67072c5a45
Tarwinder Singh <tarwinder303@gmail.com>
2021-11-12 10:30:57 +0530

    Fixed scrolling in mobile phone

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav_container").style.top = "0";
+    //document.getElementById("fixscroll").style.marginTop = "150px";
   } else {
     document.getElementById("nav_container").style.top = "-110px";
-    document.getElementById("fixscroll").style.marginTop = "140px";
+    //document.getElementById("fixscroll").style.marginTop = "140px";
   }
   prevScrollpos = currentScrollPos;
 }
